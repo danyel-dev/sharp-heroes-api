517b8d76e293694a632bc1470e1c2df2085ab7d7
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 <dannykill4221x@gmail.com>
2023-04-13 16:13:55 -0300

    a

--- sharpHeroesAPI/sharpHeroesAPI/Controllers/HeroController.cs
+++ sharpHeroesAPI/sharpHeroesAPI/Controllers/HeroController.cs
@@ -1,113 +1,54 @@
 ﻿using sharpHeroesAPI.Model;
 using Microsoft.AspNetCore.Mvc;
+using sharpHeroesAPI.Domain;
 
 namespace sharpHeroesAPI.Controllers;
 
 [ApiController]
 [Route("Heroes")]
-public class HeroController
+public class HeroController : ControllerBase
 {
-    private static List<Hero> _heroes = new List<Hero> {
-        new Hero {
-            Name = "Spider-Man",
-            RealName = "Peter Parker",
-            Age = 25,
-            Power = "Web-slinging, wall-crawling, spider-sense",
-            isRetired = false,
-        },
+    private HeroContext _context;
 
-        new Hero
-        {
-            Name = "Superman",
-            RealName = "Clark Kent",
-            Age = 35,
-            Power = "Super strength, speed, durability, and flight",
-            isRetired = false
-        },
-
-        new Hero
-        {
-            Name = "Batman",
-            RealName = "Bruce Wayne",
-            Age = 40,
-            Power = "Peak physical and mental conditioning, gadgets and technology",
-            isRetired = false
-        },
-
-        new Hero
-        {
-            Name = "Wonder Woman",
-            RealName = "Diana Prince",
-            Age = 30,
-            Power = "Super strength, durability, speed, and flight, lasso of truth",
-            isRetired = false
-        },
-
-        new Hero
-        {
-            Name = "Iron Man",
-            RealName = "Tony Stark",
-            Age = 45,
-            Power = "Powered armor suit, genius-level intellect",
-            isRetired = false
-        },
-
-        new Hero
-        {
-            Name = "Captain America",
-            RealName = "Steve Rogers",
-            Age = 100,
-            Power = "Peak physical and mental conditioning, indestructible shield",
-            isRetired = false
-        },
-
-        new Hero
-        {
-            Name = "Black Widow",
-            RealName = "Natasha Romanoff",
-            Age = 35,
-            Power = "Expert martial artist, espionage skills, gadgets and technology",
-            isRetired = false
-        },
-
-        new Hero
-        {
-            Name = "Hulk",
-            RealName = "Bruce Banner",
-            Age = 45,
-            Power = "Superhuman strength, durability, and regenerative healing",
-            isRetired = false
-        },
-
-        new Hero
-        {
-            Name = "Thor",
-            RealName = "Thor Odinson",
-            Age = 1500,
-            Power = "God of Thunder, super strength, durability, and flight, Mjolnir",
-            isRetired = false
-        },
-
-        new Hero
-        {
-            Name = "Aquaman",
-            RealName = "Arthur Curry",
-            Age = 35,
-            Power = "Superhuman strength, durability, and aquatic abilities",
-            isRetired = false
-        }
-    };
+    public HeroController(HeroContext heroContext)
+    {
+        _context = heroContext;
+    }
 
     [HttpGet]
-    public List<Hero> GetAllHeroes()
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public ActionResult<IEnumerable<Hero>> GetAllHeroes([FromQuery] bool includeRetired = true)
     {
-        return _heroes;
+        var heroes = _context.Heroes.ToList();
+
+        if (heroes.Count == 0)
+            return NotFound(new {
+                message = "Cannot found any here in heroes db.",
+                moment = DateTime.UtcNow
+            });
+
+        return Ok(includeRetired ?
+            heroes :
+            heroes.Where(h => !h.isRetired));
     }
 
     [HttpPost]
-    public List<Hero> CreateNewHero([FromBody] Hero hero)
+    [Route("/{id}")]
+    public IActionResult GetHeroById(int id)
     {
-        _heroes.Add(hero);
-        return _heroes;
+        var hero = _context.Heroes.FirstOrDefault(h => h.Id == id);
+
+        if (hero == null) return NotFound();
+        return Ok(hero);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public IActionResult CreateNewHero([FromBody] Hero hero)
+    {
+        _context.Heroes.Add(hero);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetHeroById), new { id = hero.Id }, hero);
     }
 }
--- sharpHeroesAPI/sharpHeroesAPI/Domain/Model/Hero.cs
+++ sharpHeroesAPI/sharpHeroesAPI/Domain/Model/Hero.cs
@@ -1,9 +1,23 @@
-﻿namespace sharpHeroesAPI.Model;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sharpHeroesAPI.Model;
 
 public class Hero {
-    public string Name { get; set; } = null!;
-    public string RealName { get; set; } = null!;
+    [Key]
+    [Required(ErrorMessage = "Id is required")]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Name is required")]
+    public string Name { get; set; }
+    
+    public string RealName { get; set; }
+    
+    [Range(1, int.MaxValue, ErrorMessage = "The Age must be greater than 1!!")]
     public int? Age { get; set; }
-    public string Power { get; set; } = null!;
+    
+    [Required(ErrorMessage = "Power is required")]
+    public string Power { get; set; }
+    
+    [Required(ErrorMessage = "Retired status is required")]
     public bool isRetired { get; set; }
 }
